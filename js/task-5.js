e83aec1fab7a013540e6959711adc1e304333323
OksanaGukova <kseniagukova@ukr.net>
2024-03-31 19:04:46 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+ const changeColorButton = document.querySelector(".change-color");
+const spanText = document.querySelector(".color");
+changeColorButton.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanText.textContent = document.body.style.backgroundColor;
+});
+
+
